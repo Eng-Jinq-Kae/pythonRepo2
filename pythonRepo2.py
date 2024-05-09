30f89da1ea1261e2d09d90ae8587f55a8a169daa
Eng-Jinq-Kae <116470236+Eng-Jinq-Kae@users.noreply.github.com>
2024-05-09 09:06:05 +0800

    Add files via upload

---
+++ pythonRepo2.py
@@ -0,0 +1,10 @@
+print('Insert your digit 1: ')
+x_inp_1 = input()
+x1 = int(x_inp_1)
+
+print('Insert your digit 2: ')
+x_inp_2 = input()
+x2 = int(x_inp_2)
+
+c1 = x1 + x2
+print('Total Sum: ', c1 )
