ea58fcb71da61a4f5d4e1bdf24e02fa272c2af58
Eng, Jinq Kae <jinq.kae.eng@intel.com>
2024-05-09 10:55:05 +0800

    add x4

--- pythonRepo2.py
+++ pythonRepo2.py
@@ -10,7 +10,11 @@ print('Insert your digit 3: ')
 x_inp_3 = input()
 x3 = int(x_inp_3)
 
-c1 = x1 + x2 + x3
+print('Insert your digit 4: ')
+x_inp_4 = input()
+x4 = int(x_inp_4)
+
+c1 = x1 + x2 + x3 + x4
 print('Total Sum: ', c1 )
 
 c2 = x1 * x2
